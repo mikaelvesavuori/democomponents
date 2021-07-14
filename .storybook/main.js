c7e759b015ceac684823f3aaf00395ca10485203
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2021-07-14 10:08:01 +0200

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|mdx)"
+  ],
+  "addons": [
+    {
+      name: "@storybook/addon-docs",
+      options: { transcludeMarkdown: true },
+    },
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+};
